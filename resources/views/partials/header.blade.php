a6bcad0c87b15aaa42bd7cac6b09c7084bf73bb1
mridulchow <mridul_uits@yahoo.com>
2017-08-29 20:14:19 +0600

    header file modified

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="{{ route('product.index') }}">Laravel Shopping Cart</a>
+        <a class="navbar-brand" href="{{ route('product.index') }}">Laravel Shopping Cart(Mridul)</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav navbar-right">
